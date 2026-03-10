6a266478f881529f0d1f6494815a8b02fbf70874
Devilish-Ana <ana.brito28@escola.pr.gov.br>
2026-03-10 09:19:36 -0300

    Create LIVROS table and insert sample data
    Created the LIVROS table and inserted initial data.

---
+++ livraria.sql
@@ -0,0 +1,21 @@
+-- Exercício 6
+-- Criação da tabela LIVROS
+CREATE TABLE LIVROS (
+    ID_LIVRO   INT PRIMARY KEY,
+    NOME_LIVRO VARCHAR(100),
+    AUTORIA    VARCHAR(100),
+    EDITORA    VARCHAR(100),
+    CATEGORIA  VARCHAR(50),
+    PREÇO      DECIMAL(10,2)
+);
+
+-- Inserindo valores fora de ordem
+INSERT INTO LIVROS 
+(CATEGORIA, AUTORIA, NOME_LIVRO, EDITORA, ID_LIVRO, PREÇO)
+VALUES
+('Biografia' , 'Malala Yousafzai', 'Eu sou Malala'       , 'Companhia das Letras', 11, 22.32),
+('Biografia' , 'Michelle Obama'  , 'Minha história'      , 'Objetiva'            , 12, 57.90),
+('Biografia' , 'Anne Frank'      , 'Diário de Anne Frank', 'Pe Da Letra'         , 13, 34.90);
+
+-- Exibir a tabela completa após inserção
+SELECT * FROM LIVROS;
